b4f1fdfdba7ec639e55e79b5596cb1a9e155f3fe
Shahad Al Mashaan <shahad@Shahads-MacBook-Pro-2.local>
2021-09-21 16:37:41 +0300

    calc works

--- calculator.js
+++ calculator.js
@@ -2,5 +2,27 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
-const name = prompt("What's your name? ");
-console.log(`Hi, I'm ${name}.`);
+
+const firstNum = prompt("Please enter number: ")
+const op = prompt("Please enter an operation(+,-,*,/): ")
+const secondNum = prompt("Please enter a second number: ")
+if (isNaN(firstNum) || isNaN(secondNum)) {
+    console.log("Invalid Entry")
+}
+let answer = 0
+
+if (op == "+"){
+    answer = parseInt(firstNum) + parseInt(secondNum)
+}
+else if (op == "-"){
+    answer = parseInt(firstNum) - parseInt(secondNum)
+}
+else if (op == "*"){
+    answer = parseInt(firstNum) * parseInt(secondNum)
+}
+else if (op == "/"){
+    answer = parseInt(firstNum) / parseInt(secondNum)
+}
+
+console.log(`${firstNum}${op}${secondNum} = ${answer}`)
+
