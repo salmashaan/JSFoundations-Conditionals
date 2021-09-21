eba20d6ae53d78f8c1804476f8a46ddee1d3a351
Shahad Al Mashaan <shahad@Shahads-MacBook-Pro-2.local>
2021-09-21 17:09:27 +0300

    calc complete

--- calculator.js
+++ calculator.js
@@ -3,26 +3,31 @@ const prompt = require("prompt-sync")(); // Please don't touch me :)
 // The prompt method allows us to take an input from the user.
 // Run the code to see how to use prompt.
 
-const firstNum = prompt("Please enter number: ")
+const firstNum = +prompt("Please enter a number: ")
 const op = prompt("Please enter an operation(+,-,*,/): ")
-const secondNum = prompt("Please enter a second number: ")
+const secondNum = +prompt("Please enter a second number: ")
 if (isNaN(firstNum) || isNaN(secondNum)) {
     console.log("Invalid Entry")
 }
+else {
 let answer = 0
 
-if (op == "+"){
-    answer = parseInt(firstNum) + parseInt(secondNum)
+if (op === "+"){
+    answer = firstNum + secondNum
+    console.log(`${firstNum}${op}${secondNum} = ${answer}`)
 }
-else if (op == "-"){
-    answer = parseInt(firstNum) - parseInt(secondNum)
+else if (op === "-"){
+    answer = firstNum - secondNum
+    console.log(`${firstNum}${op}${secondNum} = ${answer}`)
 }
-else if (op == "*"){
-    answer = parseInt(firstNum) * parseInt(secondNum)
+else if (op === "*"){
+    answer = firstNum * secondNum
+    console.log(`${firstNum}${op}${secondNum} = ${answer}`)
 }
-else if (op == "/"){
-    answer = parseInt(firstNum) / parseInt(secondNum)
+else if (op === "/"){
+    answer = firstNum / secondNum
+    console.log(`${firstNum}${op}${secondNum} = ${answer}`)
 }
-
-console.log(`${firstNum}${op}${secondNum} = ${answer}`)
-
+else { console.log("Invalid operation")
+}
+}
